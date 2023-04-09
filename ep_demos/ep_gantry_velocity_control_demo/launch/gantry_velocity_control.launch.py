ed308d64cfedcc6c854312ac3710c6fefe760f12
R. Nick Vandemark <rnvandemark@gmail.com>
2023-04-09 11:23:19 -0400

    Add gantry velocity control demo files, copied from / inspired by the gazebo_ros2_control_demos package.
    From the repository hosted at https://github.com/ros-controls/gazebo_ros2_control.git.
    
    'launch/gantry_velocity_control.launch.py' is a rework of the launch file in ep_demos/ep_diff_drive_demo,
    but is also inspired by gazebo_ros2_control/gazebo_ros2_control_demos/launch/cart_example_velocity.launch.py.
    
    The files in the 'urdf' directory are a copy and rework of gazebo_ros2_control/gazebo_ros2_control_demos/launch/test_cart_velocity.xacro.urdf.
    
    This also allows for configurable controller parameters for the demo.
    
    Sample use:
    ros2 launch ep_gantry_velocity_control_demo gantry_velocity_control.launch.py controller_parameters_path:=/opt/ros/galactic/share/gazebo_ros2_control_demos/config/cartpole_controller_velocity.yaml

---
+++ ep_demos/ep_gantry_velocity_control_demo/launch/gantry_velocity_control.launch.py
@@ -0,0 +1,110 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    controller_parameters_path = LaunchConfiguration("controller_parameters_path")
+    declare_controller_parameters_path_cmd = DeclareLaunchArgument(
+        "controller_parameters_path",
+        default_value=PathJoinSubstitution([
+            FindPackageShare("ep_gantry_velocity_control_demo"),
+            "config",
+            "sample.yaml",
+        ]),
+        description="The path to the YAML file of the parameters for the controller to use",
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare("gazebo_ros"),
+                "launch",
+                "gazebo.launch.py",
+            ])
+        ])
+    )
+
+    robot_description = {
+        "robot_description": Command([
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ep_gantry_velocity_control_demo"), "urdf", "gantry_velocity_control.urdf.xacro"]),
+            " controller_parameters_path:=",
+            controller_parameters_path,
+        ])
+    }
+
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[robot_description],
+        output="both",
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-topic", "robot_description",
+            "-entity", "cartpole",
+        ],
+        output="both",
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "start", "joint_state_broadcaster"],
+        output="both",
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "start", "velocity_controller"],
+        output="both",
+    )
+
+    load_imu_sensor_broadcaster = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "imu_sensor_broadcaster"],
+        output="both"
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_trajectory_controller,
+                on_exit=[load_imu_sensor_broadcaster],
+            )
+        ),
+        declare_controller_parameters_path_cmd,
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+    ])
